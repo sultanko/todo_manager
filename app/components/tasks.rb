9493370f1281d9fd39312ec087aa97fb41ae5376
sultanko <egorshah@gmail.com>
2013-05-14 22:06:18 +0300

    Add beauty tasks

--- app/components/tasks.rb
+++ app/components/tasks.rb
@@ -2,5 +2,13 @@ class Tasks < Netzke::Basepack::Grid
   def configure(c)
     super
     c.model = "Task"
+    c.columns = [
+    	:done,
+    	:name,
+    	{ name: :notes, flex: 1},
+    	:priority,
+    	{ name: :due, header: "Due on"}
+    ]
+    c.scope = {done: [nil, false] }
   end
 end
